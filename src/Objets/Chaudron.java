4ee4dac42ff9445feca0706361c32ffff5c06672
Jihanebgit <jihanebattas025@gmail.com>
2025-10-06 15:55:11 +0200

    tout

---
+++ src/Objets/Chaudron.java
@@ -0,0 +1,22 @@
+package Objets;
+
+public class Chaudron {
+	private int quantitePotion;
+	private int forcePotion;
+
+	
+	public int getQuantitePotion() {
+		return quantitePotion;
+	}
+
+
+
+	public boolean resterPotion() {
+		return getQuantitePotion()==0;
+		
+	}
+	
+	public void name() {
+		
+	}
+}
---
+++ src/Personnages/Gaulois.java
@@ -0,0 +1,44 @@
+package Personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return nom;
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom +" envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force/3);
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Gaulois asterix= new Gaulois("Astérix", 8);
+		System.out.println(asterix);
+	}
+}
+
