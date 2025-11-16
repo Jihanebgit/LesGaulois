f7166616e7b71380362f4acd2282e3e17325bfbf
Jihanebgit <jihanebattas025@gmail.com>
2025-11-16 21:26:57 +0100

    les énumérés

--- src/Personnages/Romain.java
+++ src/Personnages/Romain.java
@@ -1,16 +1,54 @@
 package Personnages;
 
+import Objets.Equipement; 
+
 public class Romain {
     private String nom;
     private int force;
+    private Equipement[] equipements ;
+    private int nbEquipements;
 
     public Romain(String nom, int force) {
         this.nom = nom;
         this.force = force;
+        this.equipements= new Equipement[2];
+        this.nbEquipements= 0;
         assert isInvariantVerified() : "Erreur Invariant ";
     }
+    
+    
+    public void sEquiper(Equipement equipement) {
+        switch (nbEquipements) {
+            case 0:
+                equipements[0] = equipement;
+                nbEquipements++;
+                System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString() + ".");
+                break;
+                
+            case 1:
+                if (equipements[0] == equipement) {
+                    System.out.println("Le soldat " + nom + " possède déjà un " + equipement.toString() + "!");
+                } else {
+                    equipements[1] = equipement;
+                    nbEquipements++;
+                    System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString()+ ".");
+                }
+                break;
+                
+            case 2:
+                System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+                break;
+        }
+    }
+    
+    
 
-    public String getNom() {
+    public int getNbEquipement() {
+		return nbEquipements;
+	}
+
+
+	public String getNom() {
         return nom;
     }
     
@@ -72,5 +110,14 @@ public class Romain {
         minus2.recevoirCoup(1);
         System.out.println("Assertion vérifier");
         
+        System.out.println(Equipement.CASQUE);
+        System.out.println(Equipement.BOUCLIER);
+        
+        minus.sEquiper(Equipement.CASQUE);
+        minus.sEquiper(Equipement.CASQUE);
+        minus.sEquiper(Equipement.BOUCLIER);
+        minus.sEquiper(Equipement.CASQUE);
+        
+
     }
 }
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -28,8 +28,7 @@ public class TestGaulois {
 		asterix.parler("Bonjour Obélix.");
 		obelix.parler("Bonjour Astérix. Ca te dirais d'aller chasser des sangliers ?");
 		asterix.parler("Oui très bonne idée.");
-		System.out.println("Dans la forêt " + asterix.getNom() + " et " + obelix.getNom()
-				+ " tombent nez à nez sur le romain " + minus.getNom() + ".");
+		System.out.println("Dans la forêt " + asterix.getNom() + " et " + obelix.getNom()+ " tombent nez à nez sur le romain " + minus.getNom() + ".");
 		
 		for (int i=0;i<3;i++) {
 			asterix.frapper(minus);
